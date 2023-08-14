65242ca74272da8961ed895b586800bbcc3d7b7f
Mike Lee <mikemunkyu.lee@gmail.com>
2023-08-14 19:05:14 -0400

    Add avatar component

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let url: string;
+	export let size: number;
+</script>
+
+<div>
+	{#if url}
+		<img src={url} width={size} height={size} alt="Avatar" class="rounded" />
+	{:else}
+		<div style="height: {size}px; width: {size}px" />
+	{/if}
+</div>
