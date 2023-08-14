b4feaed721ae5439fd84b4d36e935c6984c2947f
Mike Lee <mikemunkyu.lee@gmail.com>
2023-08-14 19:10:51 -0400

    Add logout api route

---
+++ src/routes/api/auth/signout/+server.ts
@@ -0,0 +1,10 @@
+import { json, redirect } from '@sveltejs/kit'
+
+export const POST = async ({ locals: { supabase, getSession } }) => {
+  const session = await getSession()
+
+  if (!session) return json('No session');
+
+  await supabase.auth.signOut()
+  throw redirect(303, '/')
+}
