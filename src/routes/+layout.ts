b7080b48fcf32e5faec5eaf0fb5b28158fb0d102
Mike Lee <mikemunkyu.lee@gmail.com>
2023-08-14 19:09:59 -0400

    Pull current session and profile data on page load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,11 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import type { Profile } from '$lib/types/index.js'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 
 export const load = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
 
+  // Create a new supabase client with the server session
   const supabase = createSupabaseLoadClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
@@ -15,5 +17,17 @@ export const load = async ({ fetch, data, depends }) => {
     data: { session },
   } = await supabase.auth.getSession()
 
-  return { supabase, session }
+  let profile: Profile | null = null
+
+  if (session) {
+    const { data } = await supabase
+      .from('profiles')
+      .select(`*`)
+      .eq('id', session.user.id)
+      .single()
+
+    profile = data
+  }
+
+  return { supabase, session, profile }
 }
