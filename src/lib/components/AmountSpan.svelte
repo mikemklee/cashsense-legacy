4653d28089cfe4f351d04cf44bdd38183e9bbbaf
Mike Lee <mikemunkyu.lee@gmail.com>
2023-08-16 22:08:25 -0400

    Add AmountSpan component

---
+++ src/lib/components/AmountSpan.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let value; // raw amount in cents
+
+	const absCentAmount = Math.abs(value);
+	const formattedDollarAmount = (absCentAmount / 100).toLocaleString();
+
+	const isNegative = value < 0;
+
+	const direction = isNegative ? '-' : '';
+	const color = isNegative ? 'text-red-400' : 'text-green-400';
+</script>
+
+<div class={color}>{direction}${formattedDollarAmount}</div>
--- src/lib/components/table/AmountCell.svelte
+++ src/lib/components/table/AmountCell.svelte
@@ -1,16 +1,11 @@
 <script>
+	import AmountSpan from '../AmountSpan.svelte';
+
 	export let getValue;
 
 	$: ({ getValue, ...rest } = $$props);
 
 	const rawCentAmount = getValue();
-	const absCentAmount = Math.abs(rawCentAmount);
-	const formattedDollarAmount = (absCentAmount / 100).toLocaleString();
-
-	const isNegative = rawCentAmount < 0;
-
-	const direction = isNegative ? '-' : '';
-	const color = isNegative ? 'text-red-400' : 'text-green-400';
 </script>
 
-<div class={color}>{direction}${formattedDollarAmount}</div>
+<AmountSpan value={rawCentAmount} />
