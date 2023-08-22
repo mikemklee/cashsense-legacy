5cf410a6bf63455313c1eec2ddd219ef5afe2847
Mike Lee <mikemunkyu.lee@gmail.com>
2023-08-21 21:32:04 -0400

    Add SearchInput component

---
+++ src/lib/components/inputs/SearchInput.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Icon from '@iconify/svelte';
+
+	export let value = '';
+	export let placeholder = '';
+	export let isDisabled = false;
+</script>
+
+<form on:submit|preventDefault class="flex relative">
+	<span class="absolute top-2 text-lg left-2 text-gray-400">
+		<Icon icon="tabler:search" />
+	</span>
+	<input
+		type="search"
+		bind:value
+		on:input
+		{placeholder}
+		disabled={isDisabled}
+		class="pl-8 pr-2 py-1 border border-gray-600 bg-transparent rounded outline-none w-[300px]
+    {isDisabled ? 'opacity-40' : ''}"
+	/>
+</form>
+
+<style>
+	input::placeholder {
+		@apply text-gray-500;
+	}
+</style>
