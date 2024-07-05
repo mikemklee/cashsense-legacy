76b54887a915c709e5c5474335dfd3420b84fa2d
Mike Lee <17537040+mikemklee@users.noreply.github.com>
2024-07-05 19:28:47 -0400

    Keep alive (#1)
    * Add workflow to keep alive
    
    * modify trigger conditions

---
+++ keep_alive.js
@@ -0,0 +1,19 @@
+
+import https from 'node:https';
+
+const keepAlive = () => {
+  https.get('https://cashsense.vercel.app/api/healthcheck', (res) => {
+
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+  
+    res.on('data', (d) => {
+      console.log('data:', d.toString());
+    });
+  
+  }).on('error', (e) => {
+    console.error(e);
+  }); 
+}
+
+keepAlive();
